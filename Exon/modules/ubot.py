8cc8c23995e04ef0689ca4f1b938c69055592cba
Darsh <darshshetty04@gmail.com>
2024-01-07 12:49:17 +0530

    Update ubot.py

--- Exon/modules/ubot.py
+++ Exon/modules/ubot.py
@@ -60,7 +60,7 @@ async def lastname(steal):
                 r = await conv.get_response()
                 response = await conv.get_response()
             except YouBlockedUserError:
-                await steal.reply("```Error, report to @AbishnoiMF```")
+                await steal.reply("```Error, report to @beerus_support_chat```")
                 return
             if r.text.startswith("Name"):
                 respond = await conv.get_response()
